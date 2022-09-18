db3dd99d9740da8e334b567637f2d794c1a1ea00
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-09-18 10:26:03 +0200

    Add PropertyList Component

--- PropertyRenting.UI/src/app/Property/property-card/property-card.component.ts
+++ PropertyRenting.UI/src/app/Property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -6,11 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-card.component.css']
 })
 export class PropertyCardComponent implements OnInit {
-  Property: any = {
-    "Id": 1,
-    "Type": "House",
-    "Price": 12000
-  };
+  @Input() Property: any = {};
 
   constructor() { }
 
--- PropertyRenting.UI/src/app/Property/property-list/property-list.component.ts
+++ PropertyRenting.UI/src/app/Property/property-list/property-list.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
+  Properties: Array<any> = [
+    {
+      "Id": 1,
+      "Name": "Name 1",
+      "Type": "House",
+      "Price": 12000
+    },
+    {
+      "Id": 2,
+      "Name": "Name 2",
+      "Type": "House 1",
+      "Price": 13000
+    },
+    {
+      "Id": 3,
+      "Name": "Name 3",
+      "Type": "House 2",
+      "Price": 14000
+    },
+  ];
 
   constructor() { }
 
